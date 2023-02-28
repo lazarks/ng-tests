d2c9e28428f56d87247a7a91533eaa2d18ae29ad
Lazar <82910765+lazarks@users.noreply.github.com>
2023-02-28 13:38:47 +0100

    ComponentFixtureAutoDetect

--- src/app/components/banner/banner.component.spec.ts
+++ src/app/components/banner/banner.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, ComponentFixtureAutoDetect } from '@angular/core/testing';
 
 import { BannerComponent } from './banner.component';
 
@@ -9,16 +9,16 @@ describe('BannerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BannerComponent ]
+      declarations: [ BannerComponent ],
+      providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BannerComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance;    
     h1 = fixture.nativeElement.querySelector('h1');
-    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -28,4 +28,17 @@ describe('BannerComponent', () => {
   it('should display original title', () => {
     expect(h1.textContent).toContain(component.bannerTitle)
   });
+
+  it('should still see original title after comp.bannerTitle change', () => {
+    const oldTitle = component.bannerTitle;
+    component.bannerTitle = "Test Title";
+    expect(h1.textContent).toContain(oldTitle);
+  });
+
+  it('should display updated title after detectChanges', () => {
+    component.bannerTitle = "Test Title";
+    fixture.detectChanges();
+    expect(h1.textContent).toContain(component.bannerTitle);
+  })
+
 });
