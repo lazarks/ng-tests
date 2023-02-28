7c0bf6545955bbacee0a753494d24daa659865ef
Lazar <82910765+lazarks@users.noreply.github.com>
2023-02-28 13:24:00 +0100

    test - display original title

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent implements OnInit {
+  bannerTitle = "test 123"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
